102b566ba28c05d3b226d861baeeec4466097c2c
umesh <umesharma.us11@gmail.com>
2026-05-25 12:13:50 +0530

    Refactored the category name for the cars from the generic 'others' tag

--- carease/app/api/getTopThree/[...getTopThree]/route.js
+++ carease/app/api/getTopThree/[...getTopThree]/route.js
@@ -1,42 +1,37 @@
-import { connect } from "@/dbconfig/dbconfig"
-import Car from "@/models/car"
-import { NextResponse } from "next/server"
+import { connect } from '@/dbconfig/dbconfig';
+import Car from '@/models/car';
+import { NextResponse } from 'next/server';
 
-connect()
+connect();
 
-export async function GET(req, content) {    
+export async function GET(req, content) {
+  try {
+    const popular = content.params.getTopThree[1];
+    const supercars = content.params.getTopThree[2];
+    const upcoming = content.params.getTopThree[3];
+    const hatchback = content.params.getTopThree[4];
 
-    try {
-        const popular = content.params.getTopThree[1]
-        const supercars = content.params.getTopThree[2]       
-        const upcoming = content.params.getTopThree[3]
-        const other = content.params.getTopThree[4]
+    const getTopThreeCars = async (category) => {
+      const cars = await Car.find({ category: category })
+        .sort({ createdAt: -1 })
+        .limit(3);
+      return cars;
+    };
 
-        const getTopThreeCars = async (category)=> {
-            const cars = await Car.find({ category: category })
-                .sort({ createdAt: -1 })
-                .limit(3)
-            return cars
-        }
+    const popularCarsData = await getTopThreeCars(popular);
+    const superCarsData = await getTopThreeCars(supercars);
+    const upcomingCarsData = await getTopThreeCars(upcoming);
+    const hatchbackCarsData = await getTopThreeCars(hatchback);
 
-        const popularCarsData = await getTopThreeCars(popular)
-        const superCarsData = await getTopThreeCars(supercars)
-        const upcomingCarsData = await getTopThreeCars(upcoming)
-        const otherCarsData = await getTopThreeCars(other)
-        
-        const result = {
-            popularCarsData,
-            superCarsData,
-            upcomingCarsData,  
-            otherCarsData,          
-        }
+    const result = {
+      popularCarsData,
+      superCarsData,
+      upcomingCarsData,
+      hatchbackCarsData,
+    };
 
-        return NextResponse.json(result, {status: 200})
-
-    } catch (err) {
-
-        return NextResponse.json({err: "Internal server error"}, {status: 500})
-        
-    }
-
+    return NextResponse.json(result, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ err: 'Internal server error' }, { status: 500 });
+  }
 }
